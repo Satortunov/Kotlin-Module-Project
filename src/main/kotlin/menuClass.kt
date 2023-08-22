3039258c6af76c641c1d8d0939996fa15b000a15
JollyRogerRu <satortunov@yandex.ru>
2023-08-22 16:25:41 +0300

    Работающее меню с выходом

--- src/main/kotlin/menuClass.kt
+++ src/main/kotlin/menuClass.kt
@@ -10,12 +10,25 @@ class MenuCreate
 
 {
     fun showMenu(menuItems : List<Menu>) : Int
-     {
-         for(item in menuItems)
-         {
-            println("${item.number} - ${item.title}")
-         }
-       println("${menuItems.size + 1} - Выход")
-       return menuItems.size
-     }
+    {
+        var exitCode: Boolean = true
+        var menuNumber: String? = "0"
+        while (exitCode) {
+
+            for (item in menuItems) {
+                println("${item.number} - ${item.title}")
+            }
+            println("${menuItems.size + 1} - Выход")
+
+            print("Введите номер элемента меню: ")
+            menuNumber = readLine()
+
+            if (menuNumber == (menuItems.size + 1).toString()) {
+
+                exitCode = false
+            }
+        }
+        return menuItems.size
+    }
+
  }
