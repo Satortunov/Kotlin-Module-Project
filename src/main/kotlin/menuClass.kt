cd808d30f7e23c52563715c7ebead598e63af60c
JollyRogerRu <satortunov@yandex.ru>
2023-08-22 16:53:24 +0300

    Работающее меню с выходом и проверкой на правильность ввода

--- src/main/kotlin/menuClass.kt
+++ src/main/kotlin/menuClass.kt
@@ -20,12 +20,25 @@ class MenuCreate
             }
             println("${menuItems.size + 1} - Выход")
 
+
             print("Введите номер элемента меню: ")
             menuNumber = readLine()
+            var rightEntry: Boolean = false
+            for (item in menuItems) {
+                if (menuNumber == item.number.toString()) {
+                    rightEntry = true
+                }
+                if (menuNumber == (item.number + 1).toString()) {
+                    rightEntry = true
+                }
+            }
+            if (rightEntry == true) {
+                if (menuNumber == (menuItems.size + 1).toString()) {
 
-            if (menuNumber == (menuItems.size + 1).toString()) {
-
-                exitCode = false
+                    exitCode = false
+                }
+            } else {
+                println("Вы ввели неправильное значение")
             }
         }
         return menuItems.size
