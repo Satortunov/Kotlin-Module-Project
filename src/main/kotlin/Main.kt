e900fd7fb254092f03b8804a0c138dcaa1cd5b38
JollyRogerRu <satortunov@yandex.ru>
2023-08-31 15:56:28 +0300

    Итерация 13. Все сначала. Работает показ меню

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,64 +3,13 @@ import WorkWithArchives.*
 
 fun main(args: Array<String>)
 {
-    var archiveWorks: ArchiveActions = ArchiveActions()
-    var notesList : MutableList<Notes> = ArrayList()
-    var archiveList : MutableList<Archive> = ArrayList()
-    var menuChoice: MenuCreate = MenuCreate()
-    var currentArchive: Archive = Archive("", notesList)
-
-    val secondMenuItems: List<Menu> = listOf(
-        Menu(0, "Создать заметку", {  }),
-        Menu(1, "Удалить заметку", { archiveWorks.deleteNotesItem(notesList) }),
+    val rootMenu: List<MenuItem> = listOf(
+        MenuItem(0, "Работа с архивами"), //Показать меню firstMenu
+        MenuItem(1, "Работа с заметками" ),//показать список архтвов и предложить выбрать архив из списка, после показать его заметки
+        MenuItem(2, "Выход" ), // завершить работу программы
     )
-
-    val firstMenuItems: List<Menu> = listOf(
-        Menu(0, "Создать архив", { archiveWorks.createArchieve(archiveList) }),
-        Menu(1, "Удалить архив", { archiveWorks.deleteArchieveItem(archiveList) }),
-    )
-
-    val mainMenuItems: List<Menu> = listOf(
-        Menu(0, "Работа с архивами",  { menuChoice.showMenu(firstMenuItems) } ),
-        Menu(1, "Работа с заметками", { menuChoice.showMenu(secondMenuItems)} ),
-    )
-
-    val listOfMenu:  List<MenuItem> = listOf(
-        MenuItem(0, mainMenuItems),
-        MenuItem(1, firstMenuItems),
-        MenuItem(2, mainMenuItems)
-    )
-
-    var currentMenuItem: MenuItem = listOfMenu[0]
-    var prevMenuItem: MenuItem = listOfMenu[0]
-    var nwextuItem: MenuItem = listOfMenu[0]
-
-    var currentNum : Int = menuChoice.showMenu(listOfMenu[0].items)
-    var prevNum : Int = 0
-    var nextNum : Int = 0
-
-
-    var stopWorkWithMenu: Boolean = true
-
-    while(stopWorkWithMenu){
-        listOfMenu[prevNum].items[currentNum].action()
-        listOfMenu[prevNum].number
-        //prevNum = currentNum
-        //currentNum = listOfMenu[currentNum].number
-
-    }
-
-
-
-
-
-
-
-    /*listOfMenu[0][0].action()
-    listOfMenu[0][1].action()
-    listOfMenu[1][0].action()
-    listOfMenu[1][1].action()
-    listOfMenu[2][0].action()
-    listOfMenu[2][1].action()*/
+    var currentMenu: Menu = Menu(0, rootMenu)
+    var itemChoice: Int = currentMenu.showMenu()
 
 }
 
--- src/main/kotlin/menuClass.kt
+++ src/main/kotlin/menuClass.kt
@@ -1,51 +1,31 @@
 package MenuClass
+import java.util.Scanner
+import kotlin.reflect.typeOf
 
-
-class Menu(val number: Int, val title: String, val action: () -> Unit)
+class MenuItem(val number: Int, val title: String)
+/*, val action: () -> Unit*/
 {
 
 }
 
-class MenuItem(val number: Int, val items: List<Menu>)
+class Menu(val numberOfMenu: Int, val items: List<MenuItem>)
 {
+    fun showMenu() : Int {
+        val scanner = Scanner(System.`in`)
+        var menuChoice: String = 0
+        var rightChoice: Boolean = false
+        while(!rightChoice) {
+            for (item in items) {
+                println("${item.number} - ${item.title}")
+            }
+           if ((menuChoice.toIntOrNull() is Int)) {
+                if ( (menuChoice.toInt() >= 0) && (menuChoice.toInt() <= items.size)) { rightChoice = true }
+            } else {
+                println(menuChoice)
+                println("Повторите ввод")
+            }
+        }
+        return menuChoice.toInt()
 
-}
-
-class MenuCreate
- (
-   // val menuItems:  List<Menu> = ArrayList()
-)
-
-{
- fun showMenu(menuItems : List<Menu>) : Int
- {
-  var exitCode: Boolean = true
-  var menuItemNumber: String? = "0"
-  var rightEntry: Boolean = false
-
-  while (exitCode) {
-
-   for (item in menuItems) {
-    println("${item.number} - ${item.title}")
-   }
-   println("${menuItems.size} - Выход")
-
-   print("Введите номер элемента меню: ")
-   menuItemNumber = readLine()
-
-
-   if (menuItemNumber.toString().toInt() is Int) {
-   for (item in menuItems) {
-      if ((menuItemNumber.toString().toInt() == item.number) || (menuItemNumber.toString().toInt() == menuItems.size)) { rightEntry = true }
     }
-   }
-   if (rightEntry == true) {
-    exitCode = false
-   } else {
-    println("Вы ввели неправильное значение")
-   }
-  }
-  return menuItemNumber.toString().toInt()
- }
-
- }
+}
