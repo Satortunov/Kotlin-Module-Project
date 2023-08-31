c2f195d2151cdc2d89b6f9e0aefd7edfb5dcc2c5
JollyRogerRu <satortunov@yandex.ru>
2023-08-31 16:02:54 +0300

    Итерация 14. Все сначала. Точно работает показ меню

--- src/main/kotlin/menuClass.kt
+++ src/main/kotlin/menuClass.kt
@@ -12,13 +12,15 @@ class Menu(val numberOfMenu: Int, val items: List<MenuItem>)
 {
     fun showMenu() : Int {
         val scanner = Scanner(System.`in`)
-        var menuChoice: String = 0
+        var menuChoice: String = ""
         var rightChoice: Boolean = false
         while(!rightChoice) {
             for (item in items) {
                 println("${item.number} - ${item.title}")
             }
-           if ((menuChoice.toIntOrNull() is Int)) {
+            print("Выберите пункт меню: ")
+            menuChoice = scanner.nextLine()
+            if ((menuChoice.toIntOrNull() is Int)) {
                 if ( (menuChoice.toInt() >= 0) && (menuChoice.toInt() <= items.size)) { rightChoice = true }
             } else {
                 println(menuChoice)
